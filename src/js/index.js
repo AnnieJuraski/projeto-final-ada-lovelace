720d31ace08e007ee6ce91048117d0f75ef40240
Annie Alves <annie.a.alves@gmail.com>
2023-05-02 09:51:42 -0300

    Biografia finalizada, implementado efeito slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,33 @@
 const toggle = document.getElementById('toggle');
 const html = document.querySelector('html');
+let theme = localStorage.getItem('theme');
 
-
-
-
-
-
-toggle.addEventListener('click', function (){
-    toggle.classList.toggle('active');
-    html.classList.toggle('dark-theme');
+if (theme != null){
     html.classList.toggle('light-theme');
+    toggle.classList.toggle('active'); 
+}
+
+
+
+
+
+
+toggle.addEventListener('click', function () {
+    toggle.classList.toggle('active');    
+    changeTheme();
 })
+
+function changeTheme(){
+    if (theme != null){
+        localStorage.removeItem('theme');
+    }else{
+        localStorage.setItem('theme', 'light-theme');
+    }
+    html.classList.toggle('light-theme');
+        
+    }
+
+
 
 
 
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,65 +1,146 @@
-const container = document.querySelector('.container');
 const sliders = document.querySelectorAll('.slider');
 const sliderCount = sliders.length;
-const button = document.querySelectorAll('botao-info');
+
+const button = document.querySelectorAll('.botao-info');
+
 const previous = document.getElementById('previous');
 const next = document.getElementById('next');
+
+
 let activeIndex = 0;
 
-
-next.addEventListener('click',() => {nextSlide});
-
 setActiveSlider(0);
+
+
+next.addEventListener('click',() => {nextSlider()});
+previous.addEventListener('click',() => {prevSlider()});
 
 
 function setActiveSlider(index) {
   sliders[activeIndex].classList.remove('active');
   sliders[index].classList.add('active');
   activeIndex = index;
+  deactivateButtons();
 }
 
 
-function nextSlide () {
+function nextSlider (repeat) {
   const nextIndex = (activeIndex + 1);
   const prevIndex = (activeIndex - 1);
 
-    sliders[activeIndex].classList.remove('active');
-    sliders[nextIndex].classList.add('active');
-
-
+  sliders[activeIndex].classList.add('prev');
+  sliders[activeIndex].classList.remove('active');
+  sliders[nextIndex].classList.remove('next');
+  sliders[nextIndex].classList.add('active');  
+  
+  button[activeIndex].classList.remove('ativo');
+  button[nextIndex].classList.add('ativo');   
+    
+  setActiveSlider(nextIndex);
 
 }
 
 
+function prevSlider () {
+  const nextIndex = (activeIndex + 1);
+  const prevIndex = (activeIndex - 1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-function shiftSliders() {
-  const nextIndex = (activeIndex + 1) % sliderCount;
-  const prevIndex = (activeIndex - 1 + sliderCount) % sliderCount;
   sliders[activeIndex].classList.add('next');
-  sliders[nextIndex].classList.add('active');
-  sliders[prevIndex].classList.remove('active');
-  sliders[prevIndex].classList.add('prev');
-  setTimeout(() => {
-    sliders[activeIndex].classList.remove('next');
-    sliders[prevIndex].classList.remove('prev');
-    setActiveSlider(nextIndex);
-  }, 500);
+  sliders[activeIndex].classList.remove('active');
+  sliders[prevIndex].classList.remove('prev');     
+  sliders[prevIndex].classList.add('active');
+
+  button[activeIndex].classList.remove('ativo');
+  button[prevIndex].classList.add('ativo');
+    
+    
+  setActiveSlider(prevIndex)
 }
 
-document.querySelector('#next').addEventListener('click', () => {
-  shiftSliders();
-});
+button.forEach((botao, indice) => {
+  botao.addEventListener('click', ()=>{
+     difference = (activeIndex - indice);
+
+    switch(difference){
+      case 1:
+        repeatFunc(prevSlider, 1);
+        break;
+
+      case 2:
+        repeatFunc(prevSlider, 2);
+        break;
+
+      case 3:
+        repeatFunc(prevSlider, 3);
+        break;
+
+      case 4:
+        repeatFunc(prevSlider, 4);
+        break;
+
+      case -1:
+        repeatFunc(nextSlider, 1);
+        break;
+      
+      case -2:
+        repeatFunc(nextSlider, 2);
+        break;
+
+      case -3:
+        repeatFunc(nextSlider, 3);
+        break;
+
+      case -4:
+        repeatFunc(nextSlider, 4);
+        break;
+
+      default:
+        break;      
+    }
+  })
+})
+
+
+function getActiveIndex() {
+  for (let i = 0; i < sliders.length; i++) {
+    if (sliders[i].classList.contains('active')) {
+      return i;
+    }
+  } // no active slider found
+}
+
+
+
+function deactivateButtons() {
+
+  if (getActiveIndex() == 0){
+    previous.classList.add('hidden')
+  }
+
+  else{
+    previous.classList.remove('hidden')
+
+  }
+
+  if(getActiveIndex() == sliderCount - 1){
+    next.classList.add('hidden')
+  }
+
+  else{
+    next.classList.remove('hidden')
+  }
+}
+
+
+
+function repeatFunc (funcName, times){
+  let i = 0
+
+ for (i=0; i<times; i++){
+  funcName();
+ }
+}
+
+
+
 
