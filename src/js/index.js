4a9adc598af0e32d77ddf2c91b27c60e8d932d2c
Annie Alves <annie.a.alves@gmail.com>
2023-05-02 21:31:12 -0300

    ajustes finais

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,42 @@
-const toggle = document.getElementById('toggle');
-const html = document.querySelector('html');
+const toggle = document.getElementById('toggle');  //Botão responsável por mudar o tema
+const html = document.querySelector('html'); 
 let theme = localStorage.getItem('theme');
 
-if (theme != null){
-    html.classList.toggle('light-theme');
-    toggle.classList.toggle('active-toggle'); 
+let lightMode = localStorage.getItem('light-theme'); 
+
+
+const enableLightMode = () => {
+  // 1. Add the class to the body
+  html.classList.add('light-theme');
+  // 2. Update lightMode in localStorage
+  localStorage.setItem('light-theme', 'enabled');
 }
 
+const disableLightMode = () => {
+  // 1. Remove the class from the body
+  html.classList.remove('light-theme');
+  // 2. Update lightMode in localStorage 
+  localStorage.setItem('light-theme', null);
+}
+ 
+// If the user already visited and enabled lightMode
+// start things off with it on
+if (lightMode === 'enabled') {
+  enableLightMode();
+  toggle.classList.toggle('active-toggle');
+}
 
-
-
-
+// When someone clicks the button
 
 toggle.addEventListener('click', function () {
-    toggle.classList.toggle('active-toggle');    
-    changeTheme();
+    toggle.classList.toggle('active-toggle');
+    lightMode = localStorage.getItem('light-theme')   
+  
+  if (lightMode !== 'enabled') {
+    enableLightMode();
+
+   
+  } else {  
+    disableLightMode(); 
+  }
 })
-
-function changeTheme(){
-    if (theme != null){
-        localStorage.removeItem('theme', 'light-theme');
-    }else{
-        localStorage.setItem('theme', 'light-theme');
-    }
-    html.classList.toggle('light-theme');
-        
-    }
-
-
-
-
-
-
-
-
-
-
